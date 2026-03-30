ead541a8e4d2afcef02b317d4492e93030682e15
grahamwobig <graham.wobig98@gmail.com>
2026-03-29 22:20:31 -0400

    add animated gradient background

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -4,30 +4,36 @@ import "../styles/Landing.css";
 function Landing() {
     return (
         <section className="homepage-container" id="landing">
-            <span className="homepage-name">Graham</span>
-            <div className="homepage-animation">
-                <FadingTypewriter 
-                    strings={[
-                        "Full Stack Developer",
-                        "Automation Engineer",
-                        "Product Designer",
-                        "Project Manager"
-                    ]}
-                    typeDelay={100}
-                    stringDelay={4000}
-                    cursorFade={500}
-                    textFade={300}
-                />
+            <div className="homepage-background">
+                <div className="homepage-gradient"></div>
             </div>
-            <div className="landing-banner">
-                <p>
-                    Full Stack Software Engineer meets Process Automation Engineer. 
-                    Motivated by the challenges of working on multi-disciplinary projects.
-                </p>
-            </div>
-            <div className="landing-buttons">
-                <span>Learn More</span>
-                <span>Contact Me</span>
+            <div className="homepage-content">
+                <span className="homepage-greeting">Hey,</span>
+                <span className="homepage-name">I'm Graham</span>
+                <div className="homepage-animation">
+                    <FadingTypewriter 
+                        strings={[
+                            "Full Stack Developer",
+                            "Automation Engineer",
+                            "Product Designer",
+                            "Project Manager"
+                        ]}
+                        typeDelay={100}
+                        stringDelay={4000}
+                        cursorFade={500}
+                        textFade={300}
+                    />
+                </div>
+                <div className="landing-banner">
+                    <p>
+                        Full Stack Software Engineer meets Process Automation Engineer. 
+                        Motivated by the challenges of working on multi-disciplinary projects.
+                    </p>
+                </div>
+                <div className="landing-buttons">
+                    <span>Learn More</span>
+                    <span>Contact Me</span>
+                </div>
             </div>
         </section>
     );
