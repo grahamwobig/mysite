e530602c619ffbcc24acb99e6a7fe3c960567402
grahamwobig <graham.wobig98@gmail.com>
2026-04-20 22:56:12 -0400

    migrated background to a component

---
+++ src/components/Background.tsx
@@ -0,0 +1,11 @@
+import "../styles/Background.css";
+
+function Background() {
+    return (
+        <div className="background">
+            <div className="background-gradient"></div>
+        </div>
+    );
+}
+
+export default Background;
